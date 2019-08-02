73e6ff926401acf38fbaae98b24e83ac42db0d79
yussan <main.yussan@gmail.com>
2019-08-02 10:17:36 +0700

    added link on thanks to box

--- components/boxs/ThanksToFooter.jsx
+++ components/boxs/ThanksToFooter.jsx
@@ -20,15 +20,25 @@ export default props => {
         <h3>TERIMAKASIH UNTUK</h3>
       </div>
       <div className="thanks-box col-12 align-center">
-        <a className="thanks-box--item" href="/ads">
+        <a
+          title="Belanja domain disini, bonusnya berlimpah, CSnya juga responsiv"
+          className="thanks-box--item"
+          target="_blank"
+          rel="noreferer noopener"
+          href="https://m.do.co/c/e4eacf5d20a5">
           <img
-            src="https://res.cloudinary.com/dhjkktmal/image/upload/v1561894898/kompetisi-id/referral/digitalocean.png"
+            src="https://res.cloudinary.com/dhjkktmal/image/upload/v1561894898/kompetisi-id/referral/domainesia.png"
             alt="Thanks Digital Ocean"
           />
         </a>
-        <a className="thanks-box--item" href="/ads">
+        <a
+          title="Digital Ocean solusi VPS dengan fitur lengkap mulai USD3/bulan"
+          className="thanks-box--item"
+          target="_blank"
+          rel="noreferer noopener"
+          href="https://www.domainesia.com/?aff=585">
           <img
-            src="https://res.cloudinary.com/dhjkktmal/image/upload/v1561894898/kompetisi-id/referral/domainesia.png"
+            src="https://res.cloudinary.com/dhjkktmal/image/upload/v1561894898/kompetisi-id/referral/digitalocean.png"
             alt="Thanks Domainesia"
           />
         </a>
