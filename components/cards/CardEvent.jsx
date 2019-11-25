357e9c5b79fb53121e8521c4ae04bc0cb1137bad
zmwildan <wildanzm21@gmail.com>
2019-11-25 22:59:01 +0700

    style(card event): update some styles

--- components/cards/CardEvent.jsx
+++ components/cards/CardEvent.jsx
@@ -9,76 +9,81 @@ import {
 import DayJs from '../../modules/dayjs'
 
 const CardEventStyled = Styled.div`
-padding: 0 20px;
-margin-bottom: 50px;
+  padding: 0 20px;
+  margin-bottom: 60px;
 
-.label {
-  &.label-red {
-    background-color: ${color_red_main};
-    color: ${color_white_main};
-    text-decoration: none;
-    text-align: center;
-    position: absolute;
-    top: 0;
-    width: calc(100% - 10px);
-    left: 0;
-    padding: 10px 5px;
-  }
-}
-
-.card-event-cover {
-  position: relative;
-  padding: 10px 20px;
-  height: 250px;
-  background-size: cover;
-  background-position: top center;
-  cursor: pointer;
-}
-
-.card-event-time {
-  text-align: left;
-  overflow-y: hidden;
-  height: 50px;
-  .card-event-label {
-    padding: 15px 0;
-    margin-right: 15px;
-    transition: background .5s ease;
-    font-size: 13px;
-    background-color: #FFF;
-    color: ${color_blue_main};
-    font-weight: bold;
-    text-transform: uppercase;
-    text-decoration: none;
-    display: inline-block;
-  }
-}
-
-.card-event-title {
-  height: 63px;
-  overflow: hidden;
-  h3 {
-    bottom: 0;
-    margin: 0;
-    font-size: 1.5em;
-    text-align: left;
-    a {
-      color: ${color_black_main};
-      font-weight: bold;
+  .label {
+    &.label-red {
+      background-color: ${color_red_main};
+      color: ${color_white_main};
       text-decoration: none;
+      text-align: center;
+      position: absolute;
+      top: 0;
+      width: calc(100% - 10px);
+      left: 0;
+      padding: 10px 5px;
     }
   }
-}
 
-.card-event-content {
-  text-align: left;
-  margin-bottom: 10px;
-}
+  .card-event-cover {
+    position: relative;
+    padding: 10px 20px;
+    height: 250px;
+    background-size: cover;
+    background-position: top center;
+    cursor: pointer;
+  }
 
-.card-event-category {
-  font-size: 14px;
-  color: ${color_gray_dark};
-  text-align: left;
-}
+  .card-event-time {
+    text-align: left;
+    overflow-y: hidden;
+    height: 50px;
+    .card-event-label {
+      padding: 15px 0;
+      margin-right: 15px;
+      transition: background .5s ease;
+      font-size: 13px;
+      background-color: #FFF;
+      color: ${color_blue_main};
+      font-weight: bold;
+      text-transform: uppercase;
+      text-decoration: none;
+      display: inline-block;
+    }
+  }
+
+  .card-event-title {
+    max-height: 63px;
+    overflow: hidden;
+    min-height: 35px;
+    h3 {
+      bottom: 0;
+      margin: 0;
+      font-size: 1.5em;
+      text-align: left;
+      a {
+        color: ${color_black_main};
+        font-weight: bold;
+        text-decoration: none;
+      }
+    }
+  }
+
+  .card-event-content {
+    text-align: left;
+    margin-bottom: 10px;
+    line-height: 1.5;
+    color: ${color_gray_dark};
+    max-height: 50px;
+    overflow: hidden;
+  }
+
+  .card-event-category {
+    font-size: 14px;
+    color: ${color_gray_dark};
+    text-align: left;
+  }
 `
 
 const CardEvent = props => {
