998bc1b2d945d0f96f127f07c84a5e5035547450
yussan <main.yussan@gmail.com>
2020-04-07 20:57:47 +0700

    feat(assets): replace unsplash assets to local

--- pages/bikes/index.jsx
+++ pages/bikes/index.jsx
@@ -126,7 +126,7 @@ class BikesIndex extends React.Component {
           <Header
             title={MetaData.title}
             text={MetaData.description}
-            backgroundImage="https://images.unsplash.com/photo-1545571326-00415fe9848b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+            backgroundImage="/static/images/cover/cover-bikes.jpeg"
           />
           <BikesStyled>
             <div className="grid">
