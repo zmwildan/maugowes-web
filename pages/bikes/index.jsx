d5256900afa9628d19f9299df58f5730508718b4
yussan <main.yussan@gmail.com>
2020-06-13 15:42:30 +0700

    perf(seo): handle title and desc on filter bikes

--- pages/bikes/index.jsx
+++ pages/bikes/index.jsx
@@ -1,5 +1,6 @@
 import Styled from "styled-components"
 import { connect } from "react-redux"
+import { toCamelCase } from "string-manager"
 
 // redux
 import {
@@ -63,12 +64,13 @@ class BikesIndex extends React.Component {
   componentDidMount() {
     const bikeTypes = this.props.bikes.bike_types || {}
     const bikeBrands = this.props.bikes.bike_brands || {}
+    const bikes = this.props.bikes.bike_list || {}
 
     const reqQuery = requestQueryGenerator(this.props.query)
 
     if (!bikeTypes.status) this.props.dispatch(fetchBikeTypes())
     if (!bikeBrands.status) this.props.dispatch(fetchBikeBrands())
-    this.props.dispatch(fetchBikes("bike_list", reqQuery))
+    if (!bikes.status) this.props.dispatch(fetchBikes("bike_list", reqQuery))
   }
 
   loadmoreHandler() {
@@ -88,17 +90,42 @@ class BikesIndex extends React.Component {
   }
 
   render() {
-    let { title, description } = MetaData
+    let title = "",
+      description = ""
     const { query } = this.props
+
+    const bikeTypes = this.props.bikes.bike_types || {}
+    const bikeBrands = this.props.bikes.bike_brands || {}
+    const bikes = this.props.bikes.bike_list || {}
+
     if (query.q) {
-      title = `Hasil Pencarian "${query.q}" di ${title}`
-      description = `Hasil Pencarian "${query.q}", ${description}`
+      title += `Hasil Pencarian "${query.q}" `
+      description += `Hasil Pencarian "${query.q}" `
     }
+
+    if (query.type && bikeTypes.status === 200) {
+      const typeDetail = bikeTypes.results.find((n) => n.id === query.type)
+      if (typeDetail.name) {
+        title += `${typeDetail.name} `
+        description += `${typeDetail.name} `
+      }
+    }
+
+    if (query.brand && bikeBrands.status === 200) {
+      const brandDetail = bikeBrands.results.find((n) => n.id === query.brand)
+      if (brandDetail.name) {
+        title += `${brandDetail.name} `
+        description += `${brandDetail.name} `
+      }
+    }
+
+    title += `${title ? "di " : ""}${MetaData.title}`
+    description += `${description ? "di " : ""}${MetaData.description}`
 
     return (
       <GlobalLayout
         metadata={{
-          title,
+          title: toCamelCase(title),
           description,
         }}>
         <DefaultLayout>
@@ -110,14 +137,14 @@ class BikesIndex extends React.Component {
           <BikesStyled>
             <div className="grid">
               <Sidebar
-                query={this.props.query}
+                query={query}
                 className="col-3_md-4_xs-12"
-                bikeBrands={this.props.bikes.bike_brands}
-                bikeTypes={this.props.bikes.bike_types}
+                bikeBrands={bikeBrands}
+                bikeTypes={bikeTypes}
               />
               <div className="content col-9_md-8_xs-12">
                 <BikesBox
-                  data={this.props.bikes.bike_list || {}}
+                  data={bikes}
                   loadmoreHandler={() => this.loadmoreHandler()}
                   maxResults={MaxResults}
                 />
