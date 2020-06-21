d2097bdf04a8b1d28784081b1456678d82d1d198
yussan <main.yussan@gmail.com>
2020-06-21 20:35:03 +0700

    feat(sitemap): added sitemap bike brands and bike types

--- server/handlers/sitemap.js
+++ server/handlers/sitemap.js
@@ -4,9 +4,43 @@ const videoModule = require("../modules/videos")
 const bikeModule = require("../modules/bikes")
 const DaysJs = require("dayjs")
 
+/**
+ * @see https://support.google.com/webmasters/answer/75712?hl=en
+ */
+module.exports.indexSitemap = (req, res) => {
+  res.set("Content-Type", "text/xml")
+
+  const sitemap = `
+    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <sitemap>
+        <loc>https://maugowes.com/sitemap/posts</loc>
+      </sitemap>
+      <sitemap>
+        <loc>https://maugowes.com/sitemap/videos</loc>
+      </sitemap>
+      <sitemap>
+        <loc>https://maugowes.com/sitemap/events</loc>
+      </sitemap>
+      <sitemap>
+        <loc>https://maugowes.com/sitemap/bikes</loc>
+      </sitemap>
+      <sitemap>
+        <loc>https://maugowes.com/sitemap/menus</loc>
+      </sitemap>
+      <sitemap>
+        <loc>https://maugowes.com/sitemap/bike-menus</loc>
+      </sitemap>
+      <sitemap>
+        <loc>https://maugowes.com/sitemap/bike-brands</loc>
+      </sitemap>
+    </sitemapindex>
+  `
+
+  res.send(sitemap)
+}
+
 module.exports.postsSitemap = (req, res) => {
-  // get latest 200 posts
-  req.query.limit = 200
+  req.query.limit = 1000
   req.query.page = 1
 
   postModule.fetchPosts(req, res, (json) => {
@@ -15,19 +49,19 @@ module.exports.postsSitemap = (req, res) => {
       let content = ``
       json.results.map((n) => {
         content += `
-            <url>
-            <loc>
-            https://maugowes.com${n.link}
-            </loc>
-            <lastmod>${DaysJs(n.created_on * 1000).format(
-              "YYYY-MM-DD"
-            )}</lastmod>
-            <changefreq>monthly</changefreq>
-            <priority>0.9</priority>
-            </url>
-        `
+              <url>
+                <loc>
+                https://maugowes.com${n.link}
+                </loc>
+                <lastmod>${DaysJs(n.created_on * 1000).format(
+                  "YYYY-MM-DD"
+                )}</lastmod>
+                <changefreq>monthly</changefreq>
+                <priority>0.9</priority>
+              </url>
+          `
       })
-      let sitemap = `
+      const sitemap = `
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${content}
     </urlset>
@@ -40,8 +74,7 @@ module.exports.postsSitemap = (req, res) => {
 }
 
 module.exports.videosSitemap = (req, res) => {
-  // get latest 200 posts
-  req.query.limit = 200
+  req.query.limit = 1000
   req.query.page = 1
 
   videoModule.fetchVideos(req, res, (json) => {
@@ -75,8 +108,7 @@ module.exports.videosSitemap = (req, res) => {
 }
 
 module.exports.eventsSitemap = (req, res) => {
-  // get latest 200 posts
-  req.query.limit = 200
+  req.query.limit = 1000
   req.query.page = 1
 
   eventModule.fetchEvents(req, res, (json) => {
@@ -135,7 +167,7 @@ module.exports.menusSitemap = (req, res) => {
 }
 
 module.exports.bikesSitemap = (req, res) => {
-  req.query.limit = 200
+  req.query.limit = 1000
   req.query.page = 1
 
   bikeModule.getBikes(req, res, (json) => {
@@ -152,6 +184,64 @@ module.exports.bikesSitemap = (req, res) => {
             <changefreq>monthly</changefreq>
             <priority>0.9</priority>
             </url>
+        `
+      })
+      let sitemap = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${content}
+    </urlset>
+    `
+      return res.send(sitemap)
+    } else {
+      return res.json(json)
+    }
+  })
+}
+
+module.exports.bikeBrands = (req, res) => {
+  bikeModule.getBikeBrands(req, res, (json) => {
+    if (json.status === 200) {
+      res.set("Content-Type", "text/xml")
+      let content = ``
+      json.results.map((n) => {
+        content += `
+          <url>
+            <loc>
+              https://maugowes.com/bikes?brand=${n.id}
+            </loc>
+            <lastmod>2020-06-01</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>0.9</priority>
+          </url>
+        `
+      })
+      let sitemap = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${content}
+    </urlset>
+    `
+      return res.send(sitemap)
+    } else {
+      return res.json(json)
+    }
+  })
+}
+
+module.exports.bikeTypes = (req, res) => {
+  bikeModule.getBikeTypes(req, res, (json) => {
+    if (json.status === 200) {
+      res.set("Content-Type", "text/xml")
+      let content = ``
+      json.results.map((n) => {
+        content += `
+          <url>
+            <loc>
+              https://maugowes.com/bikes?type=${n.id}
+            </loc>
+            <lastmod>2020-06-01</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>0.9</priority>
+          </url>
         `
       })
       let sitemap = `
--- server/routers/sitemap.js
+++ server/routers/sitemap.js
@@ -3,6 +3,9 @@ const Router = express.Router()
 
 const SitemapHandler = require("../handlers/sitemap")
 
+// route of : /sitemap
+Router.get("/", SitemapHandler.indexSitemap)
+
 // route of : /sitemap/post
 Router.get("/posts", SitemapHandler.postsSitemap)
 
@@ -17,5 +20,11 @@ Router.get("/menus", SitemapHandler.menusSitemap)
 
 // route of : /sitemap/bikes
 Router.get("/bikes", SitemapHandler.bikesSitemap)
+
+// route of : /sitemap/bike-brands
+Router.get("/bike-brands", SitemapHandler.bikeBrands)
+
+// router of : /sitemap/bike-types
+Router.get("/bike-types", SitemapHandler.bikeTypes)
 
 module.exports = Router
