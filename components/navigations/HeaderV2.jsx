8f495f9c753ff56cd666a0fc7b1a8c9673f869e9
yussan <main.yussan@gmail.com>
2020-12-19 10:38:18 +0700

    feat(header): add fixed style after scroll

--- components/navigations/HeaderV2.jsx
+++ components/navigations/HeaderV2.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import Styled from "styled-components"
 import { color_black_main, color_blue_main, color_gray_medium } from "../Const"
-import initialDropdown from "../../modules/dropdown"
+// import initialDropdown from "../../modules/dropdown"
 
 // components
 import Link from "next/link"
@@ -10,6 +10,7 @@ import SearchModal from "../modals/SearchInput"
 
 const HeaderMobileStyle = `
 font-size: 16px;
+transition: all .5s ease;
 
 .header-menu {
   .header-menu__left {
@@ -21,16 +22,26 @@ font-size: 16px;
       margin-right: 15px;
   }
   .header-menu__right {
-    padding-left: 15px;
+    padding: 0 15px;
   }
 }
 `
 
 const HeaderStyled = Styled.header`
+    transition: top .5 ease;
     border-bottom: 1px solid ${color_gray_medium};
     margin-bottom: 50px;
-    padding: 10px 0;
+    padding: 5px 0 10px;
     font-size: 15px;
+    top: inherit;
+
+    &.fixed {
+      position: fixed;
+      top: 0;
+      z-index: 99;
+      background: #ffffffd9;
+      width: 100%;
+    }
 
     .header-menu {
         display: flex;
@@ -118,20 +129,41 @@ const AvailableMenu = [
   // },
 ]
 
-const Header = (props) => {
+let Fixed = false
+
+const Header = () => {
   const [path, setPath] = useState({})
+  const [fixed, setFixed] = useState(false)
   const [doSearch, setDoSearch] = useState(false)
   const { pathname } = path
 
-  useEffect(() => {
-    initialDropdown()
+  const toggleFixHeader = () => {
+    if (window.pageYOffset > 500 && !Fixed) {
+      Fixed = true
+      setFixed(Fixed)
+    }
 
+    if (window.pageYOffset <= 500 && Fixed) {
+      Fixed = false
+      setFixed(Fixed)
+    }
+  }
+
+  useEffect(() => {
+    // initialDropdown()
     const pathArr = window.location.pathname.split("/")
     setPath({ pathname: pathArr[1] })
+
+    // add event listener
+    document.addEventListener("scroll", toggleFixHeader, false)
+
+    return () => {
+      document.removeEventListener("scroll", toggleFixHeader, false)
+    }
   }, [])
 
   return (
-    <HeaderStyled>
+    <HeaderStyled className={`${fixed ? "fixed" : ""}`} fixed={fixed}>
       {/* search modal */}
       {doSearch ? <SearchModal onClose={() => setDoSearch(false)} /> : null}
       {/* end of search modal */}
